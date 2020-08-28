1f75f3b226101440f44f24bba22195d21e76fa8d
Johan Daniel Ortegón <johandanielortegonparra@gmail.com>
2020-08-28 01:44:02 -0500

    Tomorrow no energy

--- workshop personal web/color-desplazamiento-mensaje.js
+++ workshop personal web/color-desplazamiento-mensaje.js
@@ -1,17 +1,17 @@
+/*Uso Global*/
 function changeColor(elem) {
-    
+
     let clases = elem.classList;
     let claseAnterior = clases[0]
     console.log("Clase anterior: " + claseAnterior)
     elem.classList.remove(claseAnterior);
     elem.classList.add("cambio_color2")
-    elem.addEventListener("mouseleave", 
-    function () {
-        elem.classList.remove("cambio_color2");
-        elem.classList.add(claseAnterior)
-    })
+    elem.addEventListener("mouseleave",
+        function () {
+            elem.classList.remove("cambio_color2");
+            elem.classList.add(claseAnterior)
+        })
 }
-
 
 
 
